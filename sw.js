7ac1bbd54c9da7428f3677066c6daf5259866dfc
juyeonjuyeonjuyeon <juyeonjuyeonjuyeon@users.noreply.github.com>
2026-07-28 10:07:43 +0900

    파비콘과 설치 알림 안내 개선

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "security-board-v3";
+const CACHE = "security-board-v4";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./data/dashboard.json", "./data/history/index.json"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
